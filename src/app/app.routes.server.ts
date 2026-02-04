549580f56f6afff3c7db08c6f8da9f35233b596e
Nicolas kaenan Silveira <nicolaskaenandev@gmail.com>
2026-02-04 19:40:55 -0300

    fix:route error 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,30 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { ServerRoute, RenderMode } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'sobre',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'projetos',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'certificados',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'control',
+    renderMode: RenderMode.Server,
+  },
   {
     path: 'projeto/:id',
     renderMode: RenderMode.Server,
